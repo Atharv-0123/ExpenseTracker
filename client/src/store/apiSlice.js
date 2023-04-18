64df2018cc94a2338177d9a4108cc735d1f771d9
Atharva <atharvakhangar14@gmail.com>
2023-04-18 11:51:40 +0530

    FinalProject

--- client/src/store/apiSlice.js
+++ client/src/store/apiSlice.js
@@ -1,6 +1,6 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
-const baseURI = 'http://localhost:8000';
+const baseURI = 'http://65.2.31.218:8000';
 
 export const apiSlice = createApi({
     baseQuery: fetchBaseQuery({baseUrl: baseURI}),
